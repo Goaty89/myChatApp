cd74bdb97db4cd08915fc527b167b532865174a6
Charles <charles-yau@live.com.my>
2021-11-16 04:38:13 +0800

    Update

---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <article class="layout">
+    <header>
+      <icon-base width="40" height="40" icon-name="avatar" class="icon"
+        ><icon-avatar
+      /></icon-base>
+      <span class="user-name">Charles</span>
+    </header>
+    <section class="content">messages</section>
+    <footer>
+      <icon-base icon-name="avatar" class="emoji"
+        ><icon-emoji v-on:click="() => alert('hello')"
+      /></icon-base>
+      <icon-base icon-name="avatar" class="attachment"
+        ><icon-attachment
+      /></icon-base>
+      <input class="inputMessage" />
+      <icon-submit-btn v-on:click="() => alert('hello')" class="submitBtn" />
+    </footer>
+  </article>
+</template>
+
+<script>
+import IconBase from "./components/IconBase.vue";
+import IconAvatar from "./assets/icons/avatar.vue";
+import IconSubmitBtn from "./assets/icons/submitBtn.vue";
+import IconAttachment from "./assets/icons/attachment.vue";
+import IconEmoji from "./assets/icons/emoji.vue";
+
+export default {
+  name: "App",
+  components: {
+    IconBase,
+    IconAvatar,
+    IconSubmitBtn,
+    IconAttachment,
+    IconEmoji,
+  },
+};
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #f1f1f2eb;
+  background-color: #1e2429;
+}
+
+.layout {
+  height: 100vh;
+  display: grid;
+  grid-template-areas:
+    "a"
+    "b"
+    "c";
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 10fr 1fr;
+}
+header {
+  display: flex;
+  align-items: center;
+  padding: 16px 10px;
+  background-color: #2a2f32;
+}
+.icon {
+  padding-right: 15px;
+}
+
+.content {
+  align-self: flex-end;
+  padding-bottom: 8px;
+}
+
+footer {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  padding: 10px;
+  background-color: #1e2428;
+}
+.emoji,
+.attachment {
+  width: 30px;
+  height: 30px;
+  color: #828689;
+  /* border-radius: 50%; */
+}
+.emoji {
+  /* background: lightcoral; */
+}
+.attachment {
+  /* background: lightgoldenrodyellow; */
+}
+.inputMessage {
+  width: 100%;
+  border-radius: 20px;
+  padding: 0.5rem;
+}
+.submitBtn {
+  color: #828689;
+}
+</style>
